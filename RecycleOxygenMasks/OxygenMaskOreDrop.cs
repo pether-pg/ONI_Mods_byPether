70750435cab284eca3b66623d5efe3def8f6fabb
pether-pg <pether.pg@gmail.com>
2021-02-05 13:23:49 +0100

    Recycle Oxygen Masks
    Adding "Recycle Oxygen Masks" mod source files

---
+++ RecycleOxygenMasks/OxygenMaskOreDrop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecycleOxygenMasks
+{
+    class OxygenMaskOreDrop : KMonoBehaviour
+    {
+        public void OnDestroy()
+        {
+            if (this.gameObject == null) return;
+            int cell = Grid.PosToCell(this.gameObject.transform.GetPosition());
+            PrimaryElement primaryElement = this.gameObject.GetComponent<PrimaryElement>();
+
+            if (primaryElement == null) return;
+            Element element = primaryElement.Element;
+            float temperature = primaryElement.Temperature;
+            float mass = primaryElement.Mass;
+            byte disease_idx = primaryElement.DiseaseIdx;
+            int disease_count = primaryElement.DiseaseCount;
+
+            if (element.substance == null) return;
+            element.substance.SpawnResource(Grid.CellToPosCCC(cell, Grid.SceneLayer.Ore), mass, temperature, disease_idx, disease_count);
+        }
+    }
+}
---
+++ RecycleOxygenMasks/Patches.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace RecycleOxygenMasks
+{
+    public class Patches
+    {
+        public static class Mod_OnLoad
+        {
+            public static void OnLoad()
+            {
+            }
+        }
+
+        [HarmonyPatch(typeof(OxygenMaskConfig))]
+        [HarmonyPatch("DoPostConfigure")]
+        public class OxygenMaskConfig_DoPostConfigure_Patch
+        { 
+
+            public static void Postfix(GameObject go)
+            {
+                go.AddComponent<OxygenMaskOreDrop>();
+            }
+        }
+    }
+}
