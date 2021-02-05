3953cbf29204e23bf88799adac6bc24b4ec59ef1
pether-pg <pether.pg@gmail.com>
2021-02-05 13:08:32 +0100

    Configurable Durability
    Adding "Configurable Durability" mod source files

---
+++ ConfigurableDurability/ConfigurableDurability_Patches.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+
+namespace ConfigurableDurability
+{
+    public class ConfigurableDurability_Patches
+    {
+        public static class Mod_OnLoad
+        {
+            public static void OnLoad()
+            {
+            }
+        }
+
+        [HarmonyPatch(typeof(Durability))]
+        [HarmonyPatch("OnPrefabInit")]
+        public class Durability_OnPrefabInit_Patch
+        {
+            public static void Postfix(ref Durability __instance)
+            {
+                __instance.durabilityLossPerCycle = Settings.Instance.durabilityLossPerCycle;
+            }
+        }
+    }
+}
---
+++ ConfigurableDurability/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfigurableDurability
+{
+    class Settings
+    {
+        private static Settings _instance = null;
+
+        public static Settings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = JsonSerializer<Settings>.Deserialize();
+                if (_instance == null)
+                {
+                    _instance = new Settings();
+                    JsonSerializer<Settings>.Serialize(_instance);
+                }
+                return _instance;
+            }
+        }
+
+        public float durabilityLossPerCycle = -0.1f;
+    }
+}
