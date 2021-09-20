7eca07a7b4a711bbee50f5425c3b7fa2e414d67a
pether-pg <pether.pg@gmail.com>
2021-09-20 11:23:42 +0200

    ResearchRequirements - accidental rebuild
    ResearchRequirements - accidental rebuild

--- ResearchRequirements/Properties/AssemblyInfo.cs
+++ ResearchRequirements/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.7.24.1722")]
-[assembly: AssemblyFileVersion("2021.7.24.1722")]
+[assembly: AssemblyVersion("2021.9.19.2048")]
+[assembly: AssemblyFileVersion("2021.9.19.2048")]
