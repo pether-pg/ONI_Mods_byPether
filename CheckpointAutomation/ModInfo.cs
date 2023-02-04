dfa5fe97927a1f0a0c95ec80195ec0d23a2995f0
pether-pg <pether.pg@gmail.com>
2023-02-04 17:06:24 +0100

    CheckpointAutomation - steam release
    CheckpointAutomation - steam release

---
+++ CheckpointAutomation/ModInfo.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CheckpointAutomation
+{
+    class ModInfo : KMod.UserMod2
+    {
+        public static string Namespace { get; private set; }
+
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+
+            Namespace = GetType().Namespace;
+            Debug.Log($"{Namespace}: Loaded from: {this.mod.ContentPath}");
+            Debug.Log($"{Namespace}: DLL version: {GetType().Assembly.GetName().Version} " +
+                        $"supporting game build {this.mod.packagedModInfo.minimumSupportedBuild} ({this.mod.packagedModInfo.supportedContent})");
+        }
+    }
+}
