a1666ff6b5be43e21cf00fa574e19d2e1f85a619
pether-pg <pether.pg@gmail.com>
2023-05-30 21:31:26 +0200

    SidequestMod - initial broken screen code
    SidequestMod - initial broken screen code

---
+++ SidequestMod/QuestScreen_Patches.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine; 
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace SidequestMod
+{
+    class QuestScreen_Patches
+    {
+        static ManagementMenu.ManagementMenuToggleInfo info = new ManagementMenu.ManagementMenuToggleInfo("Quests", "OverviewUI_research_nav_icon", tooltip: "Quests - tooltip");
+        static SidequestScreen sidequestScreen;
+
+        [HarmonyPatch(typeof(ManagementMenu))]
+        [HarmonyPatch("OnPrefabInit")]
+        public class ManagementMenu_OnPrefabInit_Patch
+        {
+            public static void Postfix()
+            {
+                sidequestScreen = new SidequestScreen();
+
+                Traverse.Create(ManagementMenu.Instance).Method("AddToggleTooltip", info, null).GetValue();
+
+                Dictionary<ManagementMenu.ManagementMenuToggleInfo, ManagementMenu.ScreenData> ScreenInfoMatch;
+                ScreenInfoMatch = Traverse.Create(ManagementMenu.Instance).Field("ScreenInfoMatch").GetValue<Dictionary<ManagementMenu.ManagementMenuToggleInfo, ManagementMenu.ScreenData>>();
+
+                ScreenInfoMatch.Add(info, new ManagementMenu.ScreenData()
+                {
+                    screen = (KScreen)sidequestScreen,
+                    tabIdx = 4,
+                    toggleInfo = info,
+                    cancelHandler = (Func<bool>)null
+                });
+            }
+        }
+
+        [HarmonyPatch(typeof(KIconToggleMenu))]
+        [HarmonyPatch("Setup")]
+        [HarmonyPatch(new Type[] { typeof(IList<KIconToggleMenu.ToggleInfo>) })]
+        public class KIconToggleMenu_Setup_Patch
+        {
+            public static void Prefix(IList<KIconToggleMenu.ToggleInfo> toggleInfo)
+            {
+                if (ContainsVitals(toggleInfo))
+                    toggleInfo.Add(info);
+            }
+
+            private static bool ContainsVitals(IList<KIconToggleMenu.ToggleInfo> toggleInfo)
+            {
+                foreach (var inf in toggleInfo)
+                    if (inf.icon == "OverviewUI_vitals_icon")
+                        return true;
+                return false;
+            }
+        }
+    }
+}
