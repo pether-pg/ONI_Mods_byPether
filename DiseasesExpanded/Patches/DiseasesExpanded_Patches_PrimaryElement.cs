6b605ec56b07cc10ad97cc8a30399a86558d3d50
pether-pg <pether.pg@gmail.com>
2023-09-12 22:48:57 +0200

    DiseasesExpanded - attempt to clear save game to prevent crash
    DiseasesExpanded - attempt to clear save game to prevent crash

---
+++ DiseasesExpanded/Patches/DiseasesExpanded_Patches_PrimaryElement.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Database;
+using System;
+using UnityEngine;
+using Klei.AI;
+using System.Collections.Generic;
+
+namespace DiseasesExpanded.Patches
+{
+    class DiseasesExpanded_Patches_PrimaryElement
+    {
+        [HarmonyPatch(typeof(PrimaryElement))]
+        [HarmonyPatch(nameof(PrimaryElement.AddDisease))]
+        public static class PrimaryElement_AddDisease_Patch
+        {
+            public static void Prefix(ref byte disease_idx)
+            {
+                if (disease_idx >= Db.Get().Diseases.Count)
+                    disease_idx = byte.MaxValue;
+            }
+        }
+    }
+}
--- DiseasesExpanded/Patches/DiseasesExpanded_Patches_SaveGame.cs
+++ DiseasesExpanded/Patches/DiseasesExpanded_Patches_SaveGame.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 using Klei.AI;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DiseasesExpanded
 {
@@ -27,6 +29,38 @@ namespace DiseasesExpanded
                 __instance.gameObject.AddComponent<MutationData>();
                 __instance.gameObject.AddComponent<MedicalNanobotsData>();
                 __instance.gameObject.AddComponent<ShieldData>();
+            }
+        }
+
+        [HarmonyPatch(typeof(Game))]
+        [HarmonyPatch("OnSpawn")]
+        public class Game_OnPrefabInit_Patch
+        {
+            public static void Postfix()
+            {
+                if (Settings.Instance.PurgeMapFromDisabledGerms)
+                    GameScheduler.Instance.Schedule("Purge the Map", 0.2f, obj => ThisEntireCityMustBePurged());
+            }
+
+            private static void ThisEntireCityMustBePurged()
+            {
+                PurgeMap();
+                Debug.Log($"{ModInfo.Namespace}: Map Purged!");
+            }
+
+            private static void PurgeMap()
+            {
+                int germCount = Db.Get().Diseases.Count;
+
+                for (int x = 0; x < Grid.WidthInCells; x++)
+                    for (int y = 0; y < Grid.HeightInCells; y++)
+                    {
+                        int cell = Grid.XYToCell(x, y);
+                        if (Grid.DiseaseIdx[cell] != byte.MaxValue && Grid.DiseaseIdx[cell] >= germCount)
+                        {
+                            SimMessages.ConsumeDisease(cell, 1.0f, int.MaxValue, 0);
+                        }
+                    }
             }
         }
     }
