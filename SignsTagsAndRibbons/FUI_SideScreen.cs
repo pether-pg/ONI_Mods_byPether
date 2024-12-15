5fda073ae2b4660d2be63ef804e4c2382e1dda7a
pether-pg <pether.pg@gmail.com>
2024-12-15 01:05:29 +0100

    SignsTagsAndRibbons - Nov24 QoL fixes
    SignsTagsAndRibbons - Nov24 QoL fixes

--- SignsTagsAndRibbons/FUI_SideScreen.cs
+++ SignsTagsAndRibbons/FUI_SideScreen.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 using System;
+using System.Linq;
+using System.Text;
 using System.Collections.Generic;
 using static DetailsScreen;
 
@@ -9,25 +11,53 @@ namespace SignsTagsAndRibbons
     // Using Aki's code from https://github.com/aki-art/ONI-Mods/blob/master/Futility/FUI/SideScreen.cs
     class FUI_SideScreen
     {
-        public static void AddClonedSideScreen<T>(string name, string originalName, Type originalType)
+
+        public static GameObject AddClonedSideScreen<T>(string name, string originalName, Type originalType, SidescreenTabTypes targetTab = SidescreenTabTypes.Config)
         {
-            bool elementsReady = GetElements(out List<SideScreenRef> screens, out GameObject contentBody);
+            bool elementsReady = GetElements(out List<SideScreenRef> screens, out var tabs);
             if (elementsReady)
             {
+                GameObject contentBody = GetContentBodyForTab(targetTab, tabs);
                 var oldPrefab = FindOriginal(originalName, screens);
                 var newPrefab = Copy<T>(oldPrefab, contentBody, name, originalType);
 
-                screens.Add(NewSideScreen(name, newPrefab));
+                screens.Add(NewSideScreen(name, newPrefab, targetTab));
+                return contentBody;
             }
+            return null;
         }
 
-        private static bool GetElements(out List<SideScreenRef> screens, out GameObject contentBody)
+        public static void AddCustomSideScreen<T>(string name, GameObject prefab, SidescreenTabTypes targetTab = SidescreenTabTypes.Config)
+        {
+            bool elementsReady = GetElements(out List<SideScreenRef> screens, out var tabs);
+            if (elementsReady)
+            {
+                var newScreen = prefab.AddComponent(typeof(T)) as SideScreenContent;
+                screens.Add(NewSideScreen(name, newScreen, targetTab));
+            }
+            else
+                Debug.LogWarning($"{ModInfo.Namespace}: Couldnt add custom sidescreen {name}, sidescreen vars not found");
+        }
+
+        private static bool GetElements(out List<SideScreenRef> screens, out List<SidescreenTab> tabs)
         {
             var detailsScreen = Traverse.Create(DetailsScreen.Instance);
-            screens = detailsScreen.Field("sideScreens").GetValue<List<DetailsScreen.SideScreenRef>>();
-            contentBody = detailsScreen.Field("sideScreenConfigContentBody").GetValue<GameObject>();
+            screens = detailsScreen.Field("sideScreens").GetValue<List<SideScreenRef>>();
+            tabs = detailsScreen.Field("sidescreenTabs").GetValue<SidescreenTab[]>().ToList();
+            return screens != null && tabs != null;
+        }
 
-            return screens != null && contentBody != null;
+        private static GameObject GetContentBodyForTab(SidescreenTabTypes targetTab, List<SidescreenTab> tabs)
+        {
+            foreach (var tab in tabs)
+            {
+                if (tab.type == targetTab)
+                {
+                    return tab.bodyInstance;
+                }
+            }
+            Debug.LogWarning($"{ModInfo.Namespace}: {targetTab} not found!");
+            return null;
         }
 
         private static SideScreenContent Copy<T>(SideScreenContent original, GameObject contentBody, string name, Type originalType)
@@ -44,24 +74,22 @@ namespace SignsTagsAndRibbons
 
         private static SideScreenContent FindOriginal(string name, List<SideScreenRef> screens)
         {
-            //foreach (var screen in screens)
-            //    Debug.Log(screen.name + screen.GetType());
-
             var result = screens.Find(s => s.name == name).screenPrefab;
 
             if (result == null)
-                Debug.LogWarning("Could not find a sidescreen with the name " + name);
+                Debug.LogWarning($"{ModInfo.Namespace}: Could not find a sidescreen with the name {name}");
 
             return result;
         }
 
-        private static DetailsScreen.SideScreenRef NewSideScreen(string name, SideScreenContent prefab)
+        private static SideScreenRef NewSideScreen(string name, SideScreenContent prefab, SidescreenTabTypes targetTab)
         {
-            return new DetailsScreen.SideScreenRef
+            return new SideScreenRef
             {
                 name = name,
                 offset = Vector2.zero,
-                screenPrefab = prefab
+                screenPrefab = prefab,
+                tab = targetTab
             };
         }
 
--- SignsTagsAndRibbons/Properties/AssemblyInfo.cs
+++ SignsTagsAndRibbons/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2024.7.13.1446")]
-[assembly: AssemblyFileVersion("2024.7.13.1446")]
+[assembly: AssemblyVersion("2024.12.14.2354")]
+[assembly: AssemblyFileVersion("2024.12.14.2354")]
