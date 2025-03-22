80cbc476bea8ce8ab1da47dbbb3d7e08e5a35b5d
pether-pg <pether.pg@gmail.com>
2025-03-22 14:25:12 +0100

    ConfigurableBuildMenus - March2025 GameUpdate fix
    ConfigurableBuildMenus - March2025 GameUpdate fix

--- ConfigurableBuildMenus/Properties/AssemblyInfo.cs
+++ ConfigurableBuildMenus/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2024.11.21.2133")]
-[assembly: AssemblyFileVersion("2024.11.21.2133")]
+[assembly: AssemblyVersion("2025.3.22.1309")]
+[assembly: AssemblyFileVersion("2025.3.22.1309")]
