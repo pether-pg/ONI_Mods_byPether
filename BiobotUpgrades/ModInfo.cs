ee2740395b120c4c4b808f07a9b39ca77143f8e8
Admin <Admin@DESKTOP-I46AJNG>
2026-06-17 17:06:14 +0200

    Biobot upgrades as example

--- BiobotUpgrades/ModInfo.cs
+++ BiobotUpgrades/ModInfo.cs
@@ -12,10 +12,15 @@ namespace BiobotUpgrades
         {
             base.OnLoad(harmony);
 
-            Namespace = GetType().Namespace;
+			var requiredDlcs = this.mod.packagedModInfo.GetRequiredDlcIds(;
+			string dlcInfo = "All";
+			if(requiredDlcs != null) 
+				dlcInfo =string.Join(", ", requiredDlcs);
+
+			Namespace = GetType().Namespace;
             Debug.Log($"{Namespace}: Loaded from: {this.mod.ContentPath}");
             Debug.Log($"{Namespace}: DLL version: {GetType().Assembly.GetName().Version} " +
-                        $"supporting game build {this.mod.packagedModInfo.minimumSupportedBuild} ({this.mod.packagedModInfo.supportedContent})");
+                        $"supporting game build {this.mod.packagedModInfo.minimumSupportedBuild} ({dlcInfo})");
 
             InitalizePOptions();
         }
