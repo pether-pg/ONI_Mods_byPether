83f33ecb3d0b90ed424ebaf7feb3cd3f241c0080
pether-pg <pether.pg@gmail.com>
2026-06-21 00:36:28 +0200

    FragrantFlowers - FishFoodConfig APP update
    FragrantFlowers - FishFoodConfig APP update

--- FragrantFlowers/Patches/FragrantFlowers_Patches_Plants.cs
+++ FragrantFlowers/Patches/FragrantFlowers_Patches_Plants.cs
@@ -107,5 +107,19 @@ namespace FragrantFlowers
                 ing.possibleMaterials = ing.possibleMaterials.Append(Crop_SpinosaRoseConfig.ID);
             }
         }
+
+        [HarmonyPatch(typeof(MicrobeMusherConfig), "ConfigureBuildingTemplate")]
+        public class MicrobeMusherConfig_Patch
+        {
+            public static void Postfix(GameObject go, Tag prefab_tag)
+            {
+                var ingr = FishFoodConfig.recipe.ingredients[0];
+                ingr.possibleMaterials = ingr.possibleMaterials.Append(new Tag[] {
+                    Plant_DuskLavenderConfig.SEED_ID,
+                    Plant_SpinosaConfig.SEED_ID,
+                    Plant_RimedMallowConfig.SEED_ID
+                });
+            }
+        }
     }
 }
